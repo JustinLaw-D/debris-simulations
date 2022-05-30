4eb79f1af308e3ed58e8c877014eb95062776624
Justin <justin11002@gmail.com>
2022-05-30 15:45:44 -0700

    updated Cell.py, currently not functional

--- Cell.py
+++ Cell.py
@@ -3,9 +3,6 @@
 import numpy as np
 from BreakupModel import *
 
-G = 6.67430e-11 # gravitational constant (N*m^2/kg^2)
-Me = 5.97219e24 # mass of Earth (kg)
-
 class Cell:
     
     def __init__(self, S_i, D_i, N_i, logL_edges, chi_edges, lam, alt, dh, tau_D, tau_N, del_t=None, sigma=None, 
@@ -70,7 +67,6 @@ class Cell:
         self.del_t = del_t
         self.sigma = sigma
         self.v = v
-        self.v_orbit = np.sqrt(G*Me/self.alt) # orbital speed (on average)
         self.alpha = alpha
         self.P = P
         self.logL_edges = logL_edges
@@ -78,36 +74,40 @@ class Cell:
         self.num_L = self.N_bins.shape[0]
         self.num_chi = self.N_bins.shape[1]
         self.update_N_vals()
+        self.lethal_N = np.full(self.N_bins.shape, False) # whether or not each bin has lethal collisions
+        self.update_lethal_N()
 
-    def step(self, D_in, dt):
+    def dxdt_cell(self, D_in):
         '''
-        calculates the number of collisions and decays from each debris bin, the number
-        of decaying derelicts, and updates the numbers for S, D, C based on this
+        calculates the rate of collisions and decays from each debris bin, the rate
+        of decaying derelicts, the rate of launches/deorbits of satallites, and the
+        rate of creation of derelicts. also updates collision totals in the cell
 
         Parameter(s):
-        D_in : number of derelict satallites entering the cell from above in dt
-        dt : time step (yr)
+        D_in : rate of derelict satallites entering the cell from above (yr^(-1))
 
         Keyword Parameter(s): None
 
         Output(s):
-        D_out : number of derelict satallites exiting the cell in dt
-        N_out : matrix with the number of exiting debris from each bin in dt
-        D_dt : number of collisions between satallites in dt
-        C_dt : matrix with the number of collisions from each bin in dt
+        dSdt : rate of change of the number of live satallites in the cell (yr^(-1))
+        dDdt : rate of change of the number of derelict satallites in the cell (yr^(-1))
+        D_out : rate of derelict satallites exiting the cell (yr^(-1))
+        N_out : matrix with the rate of exiting debris from each bin (yr^(-1))
+        D_dt : rate of collisions between satallites (yr^(-1))
+        C_dt : matrix with the rate of collisions from each bin (yr^(-1))
 
         Note: Assumes that collisions with debris of L_cm < 10cm cannot be avoided
         '''
         
         S, D = self.S[-1], self.D[-1]
 
-        # compute the number of collisions from each debris type
-        coll_S = np.zeros(self.N_bins.shape, dtype=np.int64) # collisions with live satallites
-        coll_SD = 0 # collisions between live and derelict satallites
-        coll_D = np.zeros(self.N_bins.shape, dtype=np.int64) # collisions with derelict satallites
-        decay_N = np.zeros(self.N_bins.shape, dtype=np.int64) # number of debris that decay
-        decay_D = 0 # number of derelicts that decay
-        coll_DD = 0 # number of collisions between 
+        # compute the rate of collisions from each debris type
+        dSdt = np.zeros(self.N_bins.shape, dtype=np.int64) # collisions with live satallites
+        dSDdt = 0 # collisions between live and derelict satallites
+        dDdt = np.zeros(self.N_bins.shape, dtype=np.int64) # collisions with derelict satallites
+        decay_N = np.zeros(self.N_bins.shape, dtype=np.int64) # rate of debris that decay
+        decay_D = 0 # rate of derelicts that decay
+        dDDdt = 0 # number of collisions between derelict satallites
         lethal_N = np.full(self.N_bins.shape, False) # whether or not each bin has lethal collisions
 
         # handle debris events
@@ -115,33 +115,30 @@ class Cell:
             ave_L = 10**((self.logL_edges[i] + self.logL_edges[i+1])/2) # average L value for these bins
             for j in range(self.num_chi):
                 ave_chi = (self.chi_edges[j] + self.chi_edges[j+1])/2
-                nS_col, nD_col, nN_decay = self.N_events(S, D, self.N_bins[i,j], ave_L, self.tau_N[i,j], dt)
+                dSdt_loc, dDdt_loc, decay_N_loc = self.N_events(S, D, self.N_bins[i,j], ave_L, self.tau_N[i,j])
                 if is_catastrophic(self.m_s, ave_L, ave_chi, self.v) : lethal_N[i,j] = True
-                coll_S[i,j] = nS_col
-                coll_D[i,j] = nD_col
-                decay_N[i,j] = nN_decay
+                dSdt[i,j] = dSdt_loc
+                dDdt[i,j] = dDdt_loc
+                decay_N[i,j] = decay_N_loc
         
         # compute collisions involving derelicts
-        coll_SD, coll_DD, decay_D = self.D_events(S, D, dt)
+        dSDdt, dDDdt, decay_D = self.D_events(S, D)
 
-        # compute number of satallites launched and brought down
-        sat_up, sat_down, sat_down_fail = self.S_events(S, dt)
+        # compute rate of satallites launched and brought down
+        sat_up, sat_down, sat_down_fail = self.lam, S/self.del_t, (1-self.P)*S/self.del_t
 
-        # sum everything up and update values
-        # all satallite-satallite collisions are catastrophic
-        self.C_l.append(self.C_l[-1] + coll_SD + coll_DD + np.sum(coll_S[lethal_N]) + np.sum(coll_D[lethal_N]))
-        self.C_nl.append(self.C_nl[-1] + np.sum(coll_S[lethal_N == False]) + np.sum(coll_D[lethal_N == False]))
-        tot_coll_lS, tot_coll_nlS = np.sum(coll_S[lethal_N]), np.sum(coll_S[lethal_N == False])
-        tot_coll_lD = np.sum(coll_D[lethal_N])
-        self.S.append(S + sat_up - sat_down - tot_coll_lS - tot_coll_nlS - coll_SD)
-        self.D.append(D + sat_down_fail + D_in + tot_coll_nlS - tot_coll_lD - coll_SD - 2*coll_DD - decay_D)
-        return decay_D, decay_N, coll_SD + coll_DD, coll_S + coll_D
+        # sum everything up
+        D_dt = dSDdt + dDDdt
+        C_dt = dSdt + dDdt
+        dSdt = sat_up - sat_down - np.sum(dSdt) - dSDdt
+        dDdt = sat_down_fail - decay_D + D_in - np.sum(dDdt[lethal_N == False]) - dSDdt - 2*dDDdt
+        return dSdt, dDdt, decay_D, decay_N, D_dt, C_dt
 
-    def N_events(self, S, D, N, ave_L, tau, dt):
+    def N_events(self, S, D, N, ave_L, tau):
         '''
-        calculates the number of collisions between debris with the given
-        ave_L, ave_chi, and tau with stallites in a band, as well as the amount of
-        debris that decays out of the band, in a given time step dt.
+        calculates the rate of collisions between debris with the given
+        ave_L, ave_chi, and tau with stallites in a band, as well as the rate of
+        debris that decays out of the band
 
         Parameter(s):
         S : number of live satellites in the band
@@ -149,14 +146,13 @@ class Cell:
         N : number of pieces of debris in the band, of this particular type
         ave_L : average characteristic length of the debris (m)
         tau : atmospheric drag lifetime of the debris in the band (yr)
-        dt : time period (yr)
 
         Keyword Parameter(s): None
         
         Output(s):
-        nS_col : number of collisions between debris and live satellites in the time step
-        nD_col : number of collisions between debris and derelict satellites in the time step
-        nN_decay : number of debris that decay out of the shell in the time step
+        dSdt : rate of collisions between debris and live satellites
+        dDdt : rate of collisions between debris and derelict satellites
+        dNdt : rate of debris that decay out of the shell in the time step
         '''
         sigma = self.sigma/1e6 # convert to km^2
         v = self.v*365.25*24*60*60 # convert to km/yr
@@ -164,37 +160,29 @@ class Cell:
         n = N/V # number density of the debris
 
         dSdt = 0 # rate of collisions between debris and satallites (live/derelict)
-        dDdt = 0
         if ave_L < 10/100: # collisions cannot be avoided
             dSdt = n*sigma*v*S
         else: # collisions can be avoided
             dSdt = self.alpha*n*sigma*v*S
         dDdt = n*sigma*v*D
-        nS_col = dSdt*dt # convert rates to number of collisions
-        nD_col = dDdt*dt
-        # randomly decide if a fractional collision occurs
-        nS_col = rand_poisson(nS_col, mx=min(S, N))
-        nD_col = rand_poisson(nD_col, mx=min(D, N))
-        nN_decay_old = N/tau*dt # calculate decays
-        nN_decay = rand_poisson(nN_decay_old, mx=N) # randomly decide if a fractional decay occurs
-        return nS_col, nD_col, nN_decay
+        dNdt = N/tau # calculate decays
+        return dSdt, dDdt, dNdt
 
-    def D_events(self, S, D, dt):
+    def D_events(self, S, D):
         '''
-        calculates the number of collisions between derelicts with stallites in a band, 
-        as well as the amount of derelicts that decays out of the band, in a given time step dt.
+        calculates the rate of collisions between derelicts with stallites in a band, 
+        as well as the rate of derelicts that decays out of the band
 
         Parameter(s):
         S : number of live satellites in the band
         D : number of derelict satellites in the band
-        dt : time period (yr)
 
         Keyword Parameter(s): None
         
         Output(s):
-        nDD_col : number of collisions between two derelicts in the time step
-        nSD_col : number of collisions between a derelelict and live satallite in the time step
-        nD_decay : number of derelicts that decay out of the shell in the time step
+        dDDdt : rate of collisions between two derelicts ((yr)^-1)
+        dSDdt : rate of collisions between a derelelict and live satallite ((yr)^-1)
+        dDdt : rate of derelicts that decay out of the shell ((yr)^-1)
         '''
         sigma = 4*self.sigma/1e6 # convert to km^2, and account for increased cross-section
         v = self.v*365.25*24*60*60 # convert to km/yr
@@ -204,37 +192,8 @@ class Cell:
         # rate of collisions between derelicts and satallites (live/derelict)
         dSDdt = n*sigma*v*S # collisions cannot be avoided
         dDDdt = n*sigma*v*D 
-        nSD_col = dSDdt*dt # convert rates to number of collisions
-        nDD_col = dDDdt*dt
-        # randomly decide if a fractional collision occurs
-        nSD_col = rand_poisson(nSD_col, mx=min(S, D))
-        nDD_col = rand_poisson(nDD_col, mx=D/2)
-        nD_decay = D/self.tau_D*dt # calculate decays
-        nD_decay = rand_poisson(nD_decay, mx=D) # randomly decide if a fractional decay occurs
-        return nSD_col, nDD_col, nD_decay
-
-    def S_events(self, S, dt):
-        '''
-        calculates the number satallites launched, that attempt to de-orbit, and that
-        fail to de-orbit in dt
-
-        Parameter(s):
-        S : number of live satellites in the band
-        dt : time period (yr)
-
-        Keyword Parameter(s): None
-        
-        Output(s):
-        sat_up : number of satallites launched in the time step
-        sat_down : number of satallites that attempt to de-orbit in the time step
-        sat_down_fail : number of satallites that fail a de-orbit attempt in the time step
-        '''
-
-        sat_up = rand_poisson(self.lam*dt)
-        sat_down = rand_poisson(S/self.del_t*dt, mx=S)
-        sat_down_fail = rand_poisson(sat_down*(1-self.P), mx=sat_down)
-
-        return sat_up, sat_down, sat_down_fail
+        dDdt = D/self.tau_D # calculate decays
+        return dSDdt, dDDdt, dDdt
 
     def update_N_vals(self):
         '''
@@ -255,3 +214,20 @@ class Cell:
 
         self.N_l.append(new_Nl)
         self.N_nl.append(new_Nnl)
+
+    def update_lethal_N(self):
+        '''
+        updates values in lethal_N based on current m_s, v, and bins
+
+        Parameter(s): None
+
+        Keyword Parameter(s): None
+
+        Ouput(s): None
+        '''
+
+        for i in range(self.num_L):
+            ave_L = 10**((self.logL_edges[i] + self.logL_edges[i+1])/2) # average L value for these bins
+            for j in range(self.num_chi):
+                ave_chi = (self.chi_edges[j] + self.chi_edges[j+1])/2
+                self.lethal_N[i,j] = is_catastrophic(self.m_s, ave_L, ave_chi, self.v)
