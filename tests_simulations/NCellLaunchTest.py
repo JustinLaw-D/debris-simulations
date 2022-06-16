91404d9cdb7751fe419a89a620ba461bd6a4785f
JustinLaw-D <justin11002@gmail.com>
2022-06-16 15:10:51 -0700

    small bug fixes and tests

--- tests_simulations/NCellLaunchTest.py
+++ tests_simulations/NCellLaunchTest.py
@@ -16,7 +16,7 @@ S_di = [[0,0,0]]*len(alts)
 N_i = np.zeros(len(alts), np.int64)
 for i in range(len(alts)):
     N_i[i] = int(2.5e-8*V[i])
-lam = [500, 500, 100]
+lam = [500, 0, 0]
 T = 60
 def drag_lifetime_loc(hmax, hmin, a_over_m):
     return drag_lifetime(hmax, hmin, 0, 0, a_over_m=a_over_m, dt=100/(60*60*24*365.25), maxdt=0.1)
@@ -24,8 +24,8 @@ atmosphere = NCell(S_i, S_di, D_i, N_i, [600, 750, 900],  alts, dhs, lam, drag_l
 
 atmosphere.run_sim_euler(T, dt=1/5000)
 t = atmosphere.get_t()
-S0, S1, S2 = atmosphere.get_S()[0], atmosphere.get_S()[3], atmosphere.get_S()[-1]
-D0, D1, D2 = atmosphere.get_D()[0], atmosphere.get_D()[3], atmosphere.get_D()[-1]
+S0, S1, S2 = atmosphere.get_S()[0][0]
+D0, D1, D2 = atmosphere.get_D()[0][0]
 N0, N1, N2 = atmosphere.get_N()[0], atmosphere.get_N()[3], atmosphere.get_N()[-1]
 
 import matplotlib.pyplot as plt
@@ -35,15 +35,11 @@ ax1.set_xlabel('time (yr)')
 ax1.set_ylabel('log(number)')
 ax1.set_yscale('log')
 ax1.plot(t, S0, label='S0')
-ax1.plot(t, S1, label='S1')
-ax1.plot(t, S2, label='S2')
 ax1.plot(t, D0, label='D0')
-ax1.plot(t, D1, label='D1')
-ax1.plot(t, D2, label='D2')
 ax1.plot(t, N0, label='N0')
 ax1.plot(t, N1, label='N1')
 ax1.plot(t, N2, label='N2')
-ax1.set_ylim(100, 1e10)
+ax1.set_ylim(1, 1e10)
 ax1.set_xlim(0,T)
 ax1.legend()
 
