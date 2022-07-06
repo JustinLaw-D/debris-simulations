2dff9c82e67690657ba7dc352f118146643c9b5e
JustinLaw-D <justin11002@gmail.com>
2022-07-06 11:54:53 -0700

    updated simulations

--- tests_simulations/NCellLaunchSaveTest.py
+++ tests_simulations/NCellLaunchSaveTest.py
@@ -9,18 +9,22 @@ dh = 50 # height of band (km)
 alts = np.arange(600, 900+dh/2, dh)
 alt_edges = np.arange(575, 925+1, dh)
 V = 4*np.pi*dh*(R+alts)**2 # volume of band
-S_i = [[0,0,0]]*len(alts)
-D_i = [[0,0,0]]*len(alts)
-S_di = [[0,0,0]]*len(alts)
+S_i = [[0]]*len(alts)
+D_i = [[0]]*len(alts)
+S_di = [[0]]*len(alts)
+lam = []
 N_i = np.zeros(len(alts), np.int64)
 for i in range(len(alts)):
     N_i[i] = int(2.5e-8*V[i])
-lam = [1000, 1000, 1000]
+    lam.append([0])
+lam[0][0] = 1000
+lam[3][0] = 1000
+lam[-1][0] = 100
 T = 60
 def drag_lifetime_loc(hmax, hmin, a_over_m, t):
     m0 = (t*12) % 144
     return drag_lifetime(hmax, hmin, 0, 0, a_over_m=a_over_m, dt=100/(60*60*24*365.25), maxdt=0.1, m0=m0)
-atmosphere = NCell(S_i, S_di, D_i, N_i, [600, 750, 900],  alt_edges, lam, drag_lifetime_loc, need_update)
+atmosphere = NCell(S_i, S_di, D_i, N_i, alt_edges, lam, drag_lifetime_loc, need_update)
 
-atmosphere.run_sim_precor(T, mindtfactor=10000, upper=True)
+atmosphere.run_sim_precor(T, mindtfactor=1000, upper=True)
 atmosphere.save("./", "NCellLaunchData", gap=0.1, force=True)
