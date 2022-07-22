c2fdf6bf669a12338689f28280e06e917fad86f5
JustinLaw-D <justin11002@gmail.com>
2022-07-22 09:40:55 -0700

    updated simulations

--- tests_simulations/BasicStarlinkSinglePlot.py
+++ tests_simulations/BasicStarlinkSinglePlot.py
@@ -8,15 +8,23 @@ atmosphere = NCell.load('./BasicStarlinkSingleFixedData/1x0.2/')
 t = atmosphere.get_t()
 T = t[-1]
 S = []
+D = []
 S_obj = atmosphere.get_S()
+D_obj = atmosphere.get_D()
+N = atmosphere.get_N()
 for i in range(len(S_obj)):
     S_loc = []
+    D_loc = []
     for j in range(len(t)):
-        sum_loc = 0
+        sum_S_loc = 0
+        sum_D_loc = 0
         for k in range(4):
-            sum_loc += S_obj[i][k][j]
-        S_loc.append(sum_loc)
+            sum_S_loc += S_obj[i][k][j]
+            sum_D_loc += D_obj[i][k][j]
+        S_loc.append(sum_S_loc)
+        D_loc.append(sum_D_loc)
     S.append(S_loc)
+    D.append(D_loc)
 
 import matplotlib.pyplot as plt
 
@@ -26,7 +34,8 @@ ax1.set_ylabel('log(number)')
 ax1.set_yscale('log')
 for i in range(len(S)):
     ax1.plot(t, S[i], label='S'+str(i))
-ax1.set_ylim(1, 5e3)
+    ax1.plot(t, N[i], label='N'+str(i))
+ax1.set_ylim(1, 1e5)
 ax1.set_xlim(0,T)
 ax1.legend()
 
